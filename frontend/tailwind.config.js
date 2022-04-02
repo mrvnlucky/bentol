c4b6012b2ae9e63cbd145d75287e525213ca7b04
Anki Prawira <ankiprawira@gmail.com>
2022-04-03 01:19:11 +0700

    add login & register page UI

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      'blue': '#0F084B',
+      'black': '#0D0221',
+      'white': '#FFFFFF',
+      'dark-blue': '#26408B',
+      'crystal': '#A6CFD5',
+      'silver': '#C4C4C4'
+    },
+    textColor: {
+      'header': '#a35709',
+      'body': '#773e35',
+      'white': '#ffffff',
+      'blue': '#0075FF'
+    },
+    fontFamily: {
+      'nunito': ['nunito', 'sans-serif'],
+    },
+    container: {
+      center: true
+    },
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '100': '35rem'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
