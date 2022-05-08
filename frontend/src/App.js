60d61e8bd0b944c5a2f23d9be7325d6a6a1e9471
Anki Prawira <ankiprawira@gmail.com>
2022-05-08 17:11:37 +0700

    add articles page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Login from './Components/Login.js'
 import Register from './Components/Register.js'
 import EditProfile from './Components/EditProfile.js'
 import Profile from './Components/Profile.js'
+import Article from './Components/Article.js'
 import './Styles/App.css'
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/register" element={<Register />} />
           <Route path="/editprofile" element={<EditProfile />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path='/article' element={<Article />} />
         </Routes>
       </Router>
     </div>
--- frontend/src/Components/LogNavbar.js
+++ frontend/src/Components/LogNavbar.js
@@ -9,16 +9,19 @@ const LogNavbar = () => {
             <div class="container flex flex-wrap justify-between items-center mx-auto">
                 <a href="/" class="flex items-center">
                     <img src={Logo} alt='logo bentol' className='w-36' />
-                    <span class="self-center text-sm ml-8 whitespace-nowrap dark:text-white">BERANDA</span>
+                    <span class="self-center text-sm ml-8 whitespace-nowrap text-white">BERANDA</span>
+                </a>
+                <a href='/' className='absolute ml-64'>
+                    <span className='self-center ml-8 text-sm text-white'>ARTIKEL</span>
                 </a>
                 <div className='flex'>
                     <a href="/" class="flex">
                         <img src={Profile} alt='logo bentol' className='w-8' />
-                        <span class="self-center ml-2 text-sm whitespace-nowrap dark:text-white">PROFIL</span>
+                        <span class="self-center ml-2 text-sm whitespace-nowrap text-white">PROFIL</span>
                     </a>
                     <a href="/" class="ml-12 flex">
                         <img src={Exit} alt='logo bentol' className='w-8' />
-                        <span class="self-center ml-2 text-sm whitespace-nowrap dark:text-white">KELUAR</span>
+                        <span class="self-center ml-2 text-sm whitespace-nowrap text-white">KELUAR</span>
                     </a>
                 </div>
             </div>
