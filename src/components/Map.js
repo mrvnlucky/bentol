98938007a91f889c047af42de02410ed5e83edcb
devarahifz <devara.hifzhurrahman22@gmail.com>
2022-05-21 12:42:23 +0700

    Final-fixing

--- src/components/Map.js
+++ src/components/Map.js
@@ -145,7 +145,7 @@ const Map = () => {
             class="form-select appearance-none block w-full p-1 pl-2 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
             aria-label="Default select example"
           >
-            <option disabled>Pilih jenis BBM anda</option>
+            <option disabled selected>Pilih jenis BBM anda</option>
             {gas.map((gas) => (
               <option key={gas.id} value={gas.price}>
                 {gas.name}
--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -186,8 +186,8 @@ const EditProfile = () => {
                       class="form-select appearance-none block w-full p-1 pl-2 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                       aria-label="Default select example"
                     >
-                      <option disabled value={user.vehicle._id}>
-                        {user.vehicle.brand + " " + user.vehicle.name}
+                      <option selected value={user.vehicle._id}>Pilih Kendaraan
+                        {/* {user.vehicle.brand + " " + user.vehicle.name} */}
                       </option>
                       {vehicles.map((vehicle) => (
                         <option key={vehicle._id} value={vehicle._id}>
