99b25f11beb9434c95e87bbfbbee2cb3103ead41
Marvin <marvnluck@gmail.com>
2022-05-21 11:22:11 +0700

    Added static carousel

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,10 @@
 // import Navbar from "./Navbar.js";
+import Carousel from "../components/Carousel.js";
 import Map from "../components/Map.js";
 const Homepage = () => {
   return (
     <>
+      <Carousel />
       <Map />
     </>
   );
