c495c0952bbf1faa7eaab40a8732d56b5457aa87
Marvin Luckianto <marvnluck@gmail.com>
2024-05-03 17:53:28 +0700

    Revert "initial commit"

---
+++ src/pages/EditProfile.js
@@ -0,0 +1,228 @@
+// import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { reset, editProfile, getProfile } from "../features/auth/authSlice";
+import { getVehicles } from "../features/vehicle/vehicleSlice";
+import ObjectID from "bson-objectid";
+// import LogNavbar from "./LogNavbar";
+import "../styles/EditProfile.css";
+
+const EditProfile = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isSuccess, isError, message } = useSelector(
+    (state) => state.auth
+  );
+
+  const { vehicles } = useSelector((state) => state.vehicles);
+
+  const [formData, setFormData] = useState({
+    name: user.name,
+    email: user.email,
+    vehicle: user.vehicle,
+  });
+
+  const { name, email, vehicle } = formData;
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message, { position: toast.POSITION.BOTTOM_LEFT });
+    }
+
+    if (isSuccess) {
+      return;
+    }
+    dispatch(getVehicles());
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const userData = {
+      name,
+      email,
+      vehicle,
+    };
+    dispatch(editProfile(userData));
+    dispatch(reset());
+    navigate("/profile", { replace: true });
+  };
+
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const { id } = useParams();
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   getUserById();
+  // }, []);
+
+  // const getUserById = async () => {
+  //   // const userRes = await axios.get(`http://localhost:5000/user/${id}`);
+  //   const userRes = await axios.get(
+  //     `https://bentol-backend.herokuapp.com/user/${id}`
+  //   );
+
+  //   setName(userRes.data.name);
+  //   setEmail(userRes.data.email);
+  // };
+
+  // const updateUser = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const userData = {
+  //       name,
+  //       email,
+  //     };
+  //     // await axios.patch(`http://localhost:5000/user/${id}`, userData);
+  //     await axios.patch(
+  //       `https://bentol-backend.herokuapp.com//user/${id}`,
+  //       userData
+  //     );
+  //     navigate(`/profile/${id}`);
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
+  // async function register(e) {
+  //   e.preventDefault();
+
+  //   try {
+  //     const registerData = {
+  //       name,
+  //       email,
+  //       password,
+  //       passwordVerify,
+  //     };
+
+  //     await axios.post("http://localhost:5000/user/register", registerData);
+  //     // await axios.post(
+  //     //   "https://mern-auth-template-tutorial.herokuapp.com/auth/",
+  //     //   registerData
+  //     // );
+  //     await getLoggedIn();
+  //     navigate("/", { replace: true });
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }
+  return (
+    <>
+      <body className="body-bg min-h-screen md:px-0 align-middle grid font-nunito tracking-wide">
+        <div className="mt-24 mb-auto mx-24 rounded-xl shadow-2xl">
+          <div className="flex flex-col">
+            <form onSubmit={onSubmit}>
+              <div className="m-8">
+                <h1 className="font-bold text-3xl">PROFIL</h1>
+                <div className="m-3 ml-0 flex flex-row">
+                  <label for="name" className="text-2xl font-bold w-1/4">
+                    Nama
+                  </label>
+                  <input
+                    type="name"
+                    className="mb-3.5 border border-silver rounded w-1/4 p-1 pl-2 text-xl"
+                    name="name"
+                    id="name"
+                    value={name}
+                    onChange={onChange}
+                  ></input>
+                  {/* <div className="w-2/3"></div> */}
+                </div>
+                <div className="m-3 ml-0 flex flex-row">
+                  <label for="email" className="text-2xl font-bold w-1/4">
+                    E-mail
+                  </label>
+                  <input
+                    type="email"
+                    className="mb-3.5 border border-silver rounded w-1/4 p-1 pl-2 text-xl"
+                    // placeholder="Masukkan Alamat E-mail"
+                    value={email}
+                    name="email"
+                    id="email"
+                    onChange={onChange}
+                  ></input>
+                  {/* <div className="w-2/3"></div> */}
+                </div>
+                <button
+                  type="submit"
+                  class="m-3 ml-0 h-9 px-10 bg-blue hover:bg-black rounded-lg text-white place-item-start"
+                >
+                  Simpan
+                </button>
+              </div>
+
+              <div className="m-8">
+                <h1 className="font-bold text-3xl">INFO KENDARAAN</h1>
+                {/* <div className="m-3 ml-0 flex flex-row">
+                  <label for="merek" className="text-2xl font-bold w-1/4">
+                    Merk Kendaraan
+                  </label>
+                  <span className="mb-3.5 w-1/4 text-2xl font-bold text-dark-blue"></span>
+                </div> */}
+
+                <div className="m-3 ml-0 flex flex-row">
+                  <label for="model" className="text-2xl font-bold w-1/4">
+                    Model Kendaraan
+                  </label>
+                  <div class="mb-3.5 w-1/3 xl:w-96">
+                    <select
+                      onChange={onChange}
+                      name="vehicle"
+                      id="vehicle"
+                      value={vehicle}
+                      // placeholder={user.vehicle.brand + " " + user.vehicle.name}
+                      class="form-select appearance-none block w-full p-1 pl-2 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                      aria-label="Default select example"
+                    >
+                      {user.vehicle ? (
+                        <>
+                          <option value={user.vehicle._id}>
+                            {/* Pilih Kendaraan */}
+                            {user.vehicle.brand + " " + user.vehicle.name}
+                          </option>
+                          <option disabled>----------------</option>
+                        </>
+                      ) : (
+                        <>
+                          <option selected disabled>
+                            Pilih Kendaraan
+                          </option>
+                          <option disabled>----------------</option>
+                        </>
+                      )}
+                      {vehicles.map((vehicle) => (
+                        <option key={vehicle._id} value={vehicle._id}>
+                          {vehicle.brand + " " + vehicle.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  class="m-3 ml-0 h-9 px-10 bg-blue hover:bg-black rounded-lg text-white place-item-start"
+                >
+                  Simpan
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </body>
+    </>
+  );
+};
+export default EditProfile;
---
+++ src/pages/Register.js
@@ -0,0 +1,170 @@
+// import React, { useContext, useState } from "react";
+// import axios from "axios";
+// // import Navbar from "./Navbar";
+import "../styles/Register.css";
+// import { useNavigate } from "react-router-dom";
+// import AuthContext from "../context/AuthContext";
+
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+// import { FaUser } from "react-icons/fa";
+import { register, reset } from "../features/auth/authSlice";
+// import Spinner from "../components/Spinner";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message, { position: toast.POSITION.BOTTOM_LEFT });
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== password2) {
+      toast.error("Passwords do not match");
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+
+      dispatch(register(userData));
+    }
+  };
+
+  // if (isLoading) {
+  //   return <Spinner />;
+  // }
+
+  return (
+    <>
+      <body className="body-bg min-h-screen md:px-0 align-middle grid content-center font-nunito tracking-wide">
+        <div>
+          <div className="text-center font-nunito font-bold mb-2">
+            <h1 className="">DAFTAR AKUN</h1>
+          </div>
+          <div className="SignUpForm">
+            <form onSubmit={onSubmit} className="flex flex-col">
+              <div className="flex flex-row">
+                <label for="name" className="w-1/3">
+                  Nama
+                </label>
+                <input
+                  type="text"
+                  className="mb-3.5 border border-silver rounded w-2/3 p-1 pl-2"
+                  id="name"
+                  name="name"
+                  value={name}
+                  placeholder="Masukkan Nama Lengkap"
+                  onChange={onChange}
+                />
+              </div>
+              <div className="flex flex-row">
+                <label for="email" className="w-1/3">
+                  Alamat E-mail
+                </label>
+                <input
+                  type="email"
+                  className="mb-3.5 border border-silver rounded w-2/3 p-1 pl-2"
+                  id="email"
+                  name="email"
+                  value={email}
+                  placeholder="e.g example@mail.com"
+                  onChange={onChange}
+                ></input>
+              </div>
+              <div className="flex flex-row">
+                <label for="password" className="w-1/3">
+                  Sandi
+                </label>
+                <input
+                  type="password"
+                  className="mb-3.5 border border-silver rounded w-2/3 p-1 pl-2"
+                  id="password"
+                  name="password"
+                  value={password}
+                  placeholder="Masukkan Sandi"
+                  onChange={onChange}
+                ></input>
+              </div>
+              <div className="flex flex-row">
+                <label for="password" className="w-1/3">
+                  Konfirmasi Sandi
+                </label>
+                <input
+                  className="mb-3.5 border border-silver rounded w-2/3 p-1 pl-2"
+                  type="password"
+                  placeholder="Konfirmasi Sandi"
+                  id="password2"
+                  name="password2"
+                  onChange={onChange}
+                  value={password2}
+                ></input>
+              </div>
+              {/* <div className="grid place-items-center">
+                <div>
+                  <input
+                    type="checkbox"
+                    className="accent-silver bg-silver border border-black"
+                  ></input>
+                  <label for="checkbox" className="ml-2">
+                    Ingat Akun Saya
+                  </label>
+                </div>
+              </div> */}
+              <div className="grid grid-cols-3">
+                <div></div>
+                <button
+                  type="submit"
+                  class="h-9 px-10 m-2 bg-blue hover:bg-black rounded-lg text-white place-item-start"
+                >
+                  Daftar
+                </button>
+                {/* <Link to="/login">
+                  <div className=" text-blue text-center p-3 hover:underline">
+                    Punya Akun?
+                  </div>
+                </Link> */}
+              </div>
+            </form>
+          </div>
+        </div>
+      </body>
+    </>
+  );
+};
+
+export default Register;
