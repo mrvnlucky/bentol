fdc4da6b07a9788fdb888656bf1c0d7b0c4613c2
ankiprawira <anki19001@mail.unpad.ac.id>
2024-07-20 12:05:33 +0700

    optimize carousel rendering

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,58 +7,33 @@ import DexLite from "../assets/gas/Dexlite.jpg";
 import White from "../assets/white.jpg";
 
 const featuredFuel = [Pertalite, Pertamax, PertamaxTurbo, Dex, DexLite, White];
+const harga = ["Rp. 10,000", "Rp. 12,950", "Rp. 14,400", "Rp. 15,100", "Rp. 14,550", ""];
 
-const harga = [
-  "Rp. 7.650",
-  "Rp. 12.500",
-  "Rp. 14.500",
-  "Rp. 13.700",
-  "Rp. 12.950",
-  " ",
-];
-
-let count = 0;
-export default function Carousel() {
+const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-
   const fuelLength = featuredFuel.length;
+
   const handleOnNextClick = () => {
-    if (count > fuelLength) {
-      setCurrentIndex(0);
-    }
-    count = (count + fuelLength / 2) % fuelLength;
-    setCurrentIndex(count);
+    setCurrentIndex((prevIndex) => (prevIndex + 3) % fuelLength);
   };
 
   const handleOnPreviousClick = () => {
-    if (count > fuelLength) {
-      setCurrentIndex(3);
-    }
-    count = (currentIndex + fuelLength - fuelLength / 2) % fuelLength;
-    setCurrentIndex(count);
+    setCurrentIndex((prevIndex) => (prevIndex - 3 + fuelLength) % fuelLength);
   };
+
+  const getIndex = (offset) => (currentIndex + offset + fuelLength) % fuelLength;
 
   return (
     <div className="w-1/3 bg-white z-10 select-none relative m-auto mt-2 rounded-lg border border-white shadow-lg">
       <div className="flex flex-row ml-10 mr-10">
-        <div className="w-1/3">
-          <img src={`${featuredFuel[currentIndex]}`}></img>
-          <h1 className="text-right mr-10 text-lg font-extrabold text-red mb-3">
-            {harga[currentIndex]}
-          </h1>
-        </div>
-        <div className="w-1/3">
-          <img src={`${featuredFuel[currentIndex + 1]}`}></img>
-          <h1 className="text-right mr-10 text-lg font-extrabold text-red mb-3">
-            {harga[currentIndex + 1]}
-          </h1>
-        </div>
-        <div className="w-1/3">
-          <img src={`${featuredFuel[currentIndex + 2]}`}></img>
-          <h1 className="text-right mr-10 text-lg font-extrabold text-red mb-3">
-            {harga[currentIndex + 2]}
-          </h1>
-        </div>
+        {[0, 1, 2].map((offset) => (
+          <div key={offset} className="w-1/3">
+            <img src={featuredFuel[getIndex(offset)]} alt={`fuel-${offset}`} />
+            <h1 className="text-right mr-10 text-lg font-extrabold text-red mb-3">
+              {harga[getIndex(offset)]}
+            </h1>
+          </div>
+        ))}
       </div>
       <div className="absolute w-full top-1/2 transform -translate-y-1/2 px-2 flex justify-between items-center">
         <button
@@ -67,7 +42,7 @@ export default function Carousel() {
         >
           <svg
             viewBox="0 0 32 32"
-            class="icon icon-caret-left"
+            className="icon icon-caret-left"
             aria-hidden="true"
             fill="white"
           >
@@ -80,7 +55,7 @@ export default function Carousel() {
         >
           <svg
             viewBox="0 0 32 32"
-            class="icon icon-caret-right"
+            className="icon icon-caret-right"
             aria-hidden="true"
             fill="white"
           >
@@ -90,4 +65,6 @@ export default function Carousel() {
       </div>
     </div>
   );
-}
+};
+
+export default Carousel;
